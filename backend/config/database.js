61408a7d6d73c51fa6fec5119e5ae412a058db66
ahmad_alshamary <sotaalshamary@gmail.com>
2026-04-11 19:16:41 +0300

    changed env

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  const uri = process.env.DB_URI;
+  const uri = process.env.MONGODB_URI;
 
   if (!uri) {
-    throw new Error("DB_URI is not defined in .env file");
+    throw new Error("MONGODB_URI is not defined in .env file");
   }
 
   mongoose.set("strictQuery", true);
